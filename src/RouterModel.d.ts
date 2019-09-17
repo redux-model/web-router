3aa8236c69becffc9d8c152fb3aef5d3eef08733
范文华 <531362022@qq.com>
2019-09-17 12:17:51 +0800

    fix: Complete type definition

--- src/RouterModel.d.ts
+++ src/RouterModel.d.ts
@@ -3,28 +3,34 @@ import { Model } from '@redux-model/web';
 import { Reducers } from '@redux-model/web/core/utils/types';
 
 interface Data {
-    location: Location;
-    action: Action;
+  location: Location;
+  action: Action;
 }
 
 type UnsubscribeToken = string;
 
 declare class RouterModel extends Model<Data> {
-    push(path: Path, state?: LocationState): void;
-    push<S = LocationState>(location: LocationDescriptorObject<S>): void;
-    replace(path: Path, state?: LocationState): void;
-    replace<S = LocationState>(location: LocationDescriptorObject<S>): void;
-    go(index: number): void;
-    goBack(): void;
-    goForward(): void;
+  // They are properties exactly.
+  push(path: Path, state?: LocationState): void;
+  push<S = LocationState>(location: LocationDescriptorObject<S>): void;
+  replace(path: Path, state?: LocationState): void;
+  replace<S = LocationState>(location: LocationDescriptorObject<S>): void;
+  go(index: number): void;
+  goBack(): void;
+  goForward(): void;
 
-    subscribe<Params = any>(path: Path, fn: (params: Params, location: Location, action: Action) => void): UnsubscribeToken;
-    unsubscribe(token: string): void;
+  getHistory(): History;
+  subscribe<Params = any>(path: Path, fn: (params: Params, location: Location, action: Action) => void): UnsubscribeToken;
+  unsubscribe(token: string): void;
+  registerBrowser(history?: History): Reducers;
+  registerHash(history?: History): Reducers;
 
-    registerBrowser(history?: History): Reducers;
-    registerHash(history?: History): Reducers;
+  /**
+   * @deprecated Use registerBrowser() or registerHash()
+   */
+  register(): Reducers;
 
-    protected initReducer(): (() => Data) | Data;
+  protected initReducer(): (() => Data) | Data;
 }
 
 export declare const routerModel: RouterModel;
